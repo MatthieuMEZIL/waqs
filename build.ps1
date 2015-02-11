93c9ccb46d21ec523c0a09a4bc0045cb1b1bf6fa
Rick Krause <rickkr@microsoft.com>
2015-02-11 09:21:09 -0800

    Changed output

--- build.ps1
+++ build.ps1
@@ -1,14 +1,11 @@
 function Build-NuPkg([string] $nuspecFile)
 {
-    echo "Updating '$nuspecFile'"
+    echo "Building '$nuspecFile'"
 
     [xml]$nuspec = Get-Content $nuspecFile
     $digits = $nuspec.package.metadata.version -split '\.'
     $digits[3] = $($($digits[3] -as [Int32]) + 1) -as [String]
     $version = $digits -join '.'
-
-    $command =  ".\New-NuGetPackage.ps1 -NuSpecFilePath ""$nuspecFile"" -VersionNumber $version -ReleaseNotes ""Version $version"" -NoPrompt"
-    echo $command
 
     .\New-NuGetPackage.ps1 -NuSpecFilePath "$nuspecFile" -VersionNumber $version -ReleaseNotes "Version $version" -NoPrompt
 }
